40f758b6e9b543d2d9ce73ee71716e6fb2f4c06d
the-hasib <hasib.cse.uiu@gmail.com>
2025-12-11 23:07:46 +0600

    Add a code

--- Array/Reverse_order.c
+++ Array/Reverse_order.c
@@ -3,24 +3,38 @@
 int main() {
     int n;
     
-    // Taking input for the number of elements in the array
-    printf("Enter the number of elements: ");
+    // Input the number of elements
+    printf("Enter the number of integers: ");
     scanf("%d", &n);
     
-    int arr[n];
+    // Declare arrays A and B of size n
+    int A[100], B[100];
     
-    // Taking input for the array elements
-    printf("Enter %d integers: ", n);
+    // Input the integers into array A
+    printf("Enter %d integers for array A: ", n);
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        scanf("%d", &A[i]);
+         }
+    
+    // Copy elements from array A to array B in reverse order
+for (int i = 0; i < n; i++) {
+        B[i] = A[n - i - 1];
     }
     
-    // Printing the array elements in reverse order
-    printf("The integers in reverse order are: ");
-    for (int i = n - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
+    // Print array A
+    printf("Array A: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
     }
     printf("\n");
-
+    
+    // Print array B
+    printf("Array B: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", B[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
+
