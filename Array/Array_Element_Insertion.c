ada3c7ae83c2c75a547a608f2828567795b6e8b3
the-hasib <hasib.cse.uiu@gmail.com>
2025-12-15 23:53:53 +0600

    Add a code

---
+++ Array/Array_Element_Insertion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, a,b,count=0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    int arr1[n];
+    printf("Enter %d integers for array A:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    printf("Enter a number:");
+    
+        scanf("%d",&a);
+        printf("Enter a position index number:");
+        scanf("%d",&b);
+        for (int i=0;i>b;i--)
+        {
+              arr1[i] = arr1[i - 1];
+
+        }    
+            arr1[b] = a;
+//n++; 
+         printf("Updated array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr1[i]);
+    }
+
+}
