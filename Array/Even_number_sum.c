059bd0a061f1eefcbd6c70a886f82f0112e11003
the-hasib <hasib.cse.uiu@gmail.com>
2025-12-19 23:06:19 +0600

    update

--- Array/Even_number_sum.c
+++ Array/Even_number_sum.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 int main (){
     int arr[100],i,n,sum=0;
-    printf("Enter value:");
+    printf("Enter Nth array number:");
     scanf("%d",&n);
 
-
+printf("Enter value:");
     for (i=0;i<n;i++){
  scanf("%d",&arr[i]);
     }
         for (i=0;i<n;i++){
             if(arr[i]%2==0)
             sum+=arr[i];
- 
     }
     printf("%d",sum);
     
